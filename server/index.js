a915723c88f6cb98dbe377d4f5617d9d7ea5257d
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-04-25 16:21:38 +0530

    readability

--- server/index.js
+++ server/index.js
@@ -1,37 +1,48 @@
-// Declare variables used
-let app, base_url, bodyParser, client, express, port, rtg, shortid
-// Define values
-express = require('express')
-app = express()
-port = process.env.PORT || 5000
-shortid = require('shortid')
-bodyParser = require('body-parser')
-base_url = process.env.BASE_URL || 'http://localhost:5000'
+// Define variables and its values
+const express = require('express')
+const app = express()
+const port = 3000
+const shortid = require('shortid')
+const bodyParser = require('body-parser')
+const base_url = 'http://localhost:5000'
+
 // Set up connection to Redis
 if (process.env.REDISTOGO_URL) {
-  rtg = require('url').parse(process.env.REDISTOGO_URL)
-  client = require('redis').createClient(rtg.port, rtg.hostname)
+  console.log(process.env.REDISTOGO_URL)
+  const rtg = require('url').parse(process.env.REDISTOGO_URL)
+  const client = require('redis').createClient(rtg.port, rtg.hostname)
   client.auth(rtg.auth.split(':')[1])
 } else {
-  client = require('redis').createClient()
+  console.log('Creating Redis client ')
+  const client = require('redis').createClient()
 }
 // Set up templating
+
+// __dirname => where the currently executing scripts reside
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
+// Express-compliant template engines such as Jade and Pug,
+// export a function named __express(filePath, options, callback),
+// which is called by the res.render() function to render the template code.
 app.engine('jade', require('jade').__express)
+
 // Set URL
 app.set('base_url', base_url)
+
 // Handle POST data
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
 // Define index route
 app.get('/', function (req, res) {
   res.render('index')
 })
+
 // Serve static files
 app.use(express.static(__dirname + '/static'))
+
 // Listen
 app.listen(port)
 console.log('Listening on port ' + port)
