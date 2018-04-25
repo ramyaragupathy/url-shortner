3d96e2e94f41e89c0065cf8256fd7fda544c2c82
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-04-25 08:33:42 +0530

    views + app setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,37 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
-app.listen(PORT, () => {
-  console.log(`Server started on port`, PORT)
+// Declare variables used
+let app, base_url, bodyParser, client, express, port, rtg, shortid
+// Define values
+express = require('express')
+app = express()
+port = process.env.PORT || 5000
+shortid = require('shortid')
+bodyParser = require('body-parser')
+base_url = process.env.BASE_URL || 'http://localhost:5000'
+// Set up connection to Redis
+if (process.env.REDISTOGO_URL) {
+  rtg = require('url').parse(process.env.REDISTOGO_URL)
+  client = require('redis').createClient(rtg.port, rtg.hostname)
+  client.auth(rtg.auth.split(':')[1])
+} else {
+  client = require('redis').createClient()
+}
+// Set up templating
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.engine('jade', require('jade').__express)
+// Set URL
+app.set('base_url', base_url)
+// Handle POST data
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+// Define index route
+app.get('/', function (req, res) {
+  res.render('index')
 })
+// Serve static files
+app.use(express.static(__dirname + '/static'))
+// Listen
+app.listen(port)
+console.log('Listening on port ' + port)
