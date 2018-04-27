f38c1cbea3a3694da3ccb5c303dd802125190dd6
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-04-27 08:32:28 +0530

    test url submission

--- test/test.js
+++ test/test.js
@@ -7,12 +7,27 @@ const baseURL = 'http://localhost:' + port
 // TEST CASES
 test('url-shortner Test Cases', function (testcase) {
   // TEST #1
-  testcase.test('Index page', function (assert) {
+  testcase.test('Test index page load', function (assert) {
     let stringExists
     request.get(baseURL, function (error, response, body) {
-      stringExists = response.body.includes('url-shortner')
-      assert.equal(true, stringExists)
-      assert.end()
+      if (!error) {
+        stringExists = response.body.includes('url-shortner')
+        assert.equal(true, stringExists)
+        assert.end()
+      }
+    })
+  })
+
+  // TEST #2
+  testcase.test('Test submitting a URL', function (assert) {
+    let stringExists
+    let longUrl = 'https://stackoverflow.com/questions/32710847/what-is-the-best-way-to-check-for-empty-request-body'
+    request.post(baseURL, {form: {url: longUrl}}, function (error, response, body) {
+      if (!error) {
+        stringExists = response.body.includes('Your shortened URL is')
+        assert.equal(true, stringExists)
+        assert.end()
+      }
     })
   })
 })
