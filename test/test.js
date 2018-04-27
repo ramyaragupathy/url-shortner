3460ad462ba3a280e74e8fc60f8814aff0e799b9
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-04-27 07:31:23 +0530

    index page

--- test/test.js
+++ test/test.js
@@ -1,1 +1,18 @@
-var test = require('tape')
+// Declare variables
+const test = require('tape')
+const request = require('request')
+const port = 3000
+const baseURL = 'http://localhost:' + port
+
+// TEST CASES
+test('url-shortner Test Cases', function (testcase) {
+  // TEST #1
+  testcase.test('Index page', function (assert) {
+    let stringExists
+    request.get(baseURL, function (error, response, body) {
+      stringExists = response.body.includes('url-shortner')
+      assert.equal(true, stringExists)
+      assert.end()
+    })
+  })
+})
