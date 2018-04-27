d6b7739a55b61213a3fe557ce0e36101705be842
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-04-27 08:54:35 +0530

    baseurl variable

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const test = require('tape')
 const request = require('request')
 const redis = require('redis')
 const port = 3000
-const baseURL = 'http://localhost:' + port
+const baseUrl = 'http://localhost:' + port
 const client = redis.createClient()
 
 // TEST CASES
@@ -11,7 +11,7 @@ test('url-shortner Test Cases', function (testcase) {
   // TEST #1
   testcase.test('Test index page load', function (assert) {
     let stringExists
-    request.get(baseURL, function (error, response, body) {
+    request.get(baseUrl, function (error, response, body) {
       if (!error) {
         stringExists = response.body.includes('url-shortner')
         assert.equal(true, stringExists)
@@ -24,7 +24,7 @@ test('url-shortner Test Cases', function (testcase) {
   testcase.test('Test submitting a URL', function (assert) {
     let stringExists
     let longUrl = 'https://stackoverflow.com/questions/32710847/what-is-the-best-way-to-check-for-empty-request-body'
-    request.post(baseURL, {form: {url: longUrl}}, function (error, response, body) {
+    request.post(baseUrl, {form: {url: longUrl}}, function (error, response, body) {
       if (!error) {
         stringExists = response.body.includes('Your shortened URL is')
         assert.equal(true, stringExists)
@@ -37,7 +37,7 @@ test('url-shortner Test Cases', function (testcase) {
   testcase.test('Test URL redirection', function (assert) {
     client.set('test', 'https://www.google.com', function () {
       request.get({
-        url: 'http://localhost:3000/testurl',
+        url: baseUrl + '/testurl',
         followRedirect: false
       }, function (error, response, body) {
         if (!error) {
@@ -50,16 +50,18 @@ test('url-shortner Test Cases', function (testcase) {
   })
 
   // TEST 4
-  testcase.test('Check nonexistent URL', function(assert){
+  testcase.test('Check nonexistent URL', function (assert) {
+    let stringExists
     request.get({
-        url: 'http://localhost:3000/nonexistenturl',
-        followRedirect: false
-      }, function (error, response, body) {
-        if (!error) {
-          assert.equal(response.headers.location, 'http://www.google.com')
-          assert.equal(response.statusCode, 404)
-          assert.end()
-        }
-      })
+      url: baseUrl + '/nonexistenturl',
+      followRedirect: false
+    }, function (error, response, body) {
+      if (!error) {
+        stringExists = response.body.includes('Link not found')
+        assert.equal(response.statusCode, 404)
+        assert.equal(true, stringExists)
+        assert.end()
+      }
+    })
   })
 })
